f37d55b40b63c86acac49b5a836c3c4aa8d87ea4
Tomek <tomekm7@gmail.com>
2022-08-16 22:12:57 +0200

    added swagger annotations

--- server/Program.cs
+++ server/Program.cs
@@ -7,7 +7,10 @@ builder.Services.AddSingleton<IGiftsRepository, InMemGiftsRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.EnableAnnotations();
+});
 
 var app = builder.Build();
 
