decde2fe99a40de326688c6a600adbf1a7214baf
Tomek <tomekm7@gmail.com>
2022-08-27 22:25:09 +0200

    added GiftController test

--- server/Prezentex.UnitTests/Prezentex.UnitTests/ControllerTests/GiftsControllerTests.cs
+++ server/Prezentex.UnitTests/Prezentex.UnitTests/ControllerTests/GiftsControllerTests.cs
@@ -6,19 +6,22 @@ using Prezentex.Api.Entities;
 using Prezentex.Api.Controllers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Prezentex.Api.Dtos;
+using FluentAssertions;
 
 namespace Prezentex.UnitTests.ControllerTests
 {
     public class GiftsControllerTests
     {
-
         //naming convention: UnitOfWork_StateUnderTest_ExpectedBehavior
+
+        private readonly Mock<IGiftsRepository> repositoryStub = new();
+        private readonly Random rand = new Random();
 
         [Fact]
         public async Task GetGiftAsync_WithUnexistingGift_ReturnsNotFound()
         {
             //Arrange
-            var repositoryStub = new Mock<IGiftsRepository>();
             repositoryStub.Setup(repo => repo.GetGiftAsync(It.IsAny<Guid>()))
                 .ReturnsAsync((Gift)null);
 
@@ -28,7 +31,43 @@ namespace Prezentex.UnitTests.ControllerTests
             var result = await controller.GetGiftAsync(Guid.NewGuid());
 
             //Assert
-            Assert.IsType<NotFoundResult>(result.Result);
+            result.Result.Should().BeOfType<NotFoundResult>();
+        }
+
+        [Fact]
+        public async Task GetGiftAsync_WithExistingGift_ReturnsExpectedGift()
+        {
+            //Arrange
+            var expectedGift = CreateRandomGift();
+            repositoryStub.Setup(repo => repo.GetGiftAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(expectedGift);
+            var controller = new GiftsController(repositoryStub.Object);
+
+            //Act
+            var result = await controller.GetGiftAsync(Guid.NewGuid());
+
+            //Assert
+            result.Value.Should().BeEquivalentTo(
+                expectedGift,
+                options =>
+                {
+                    options
+                    .ComparingByMembers<Gift>()
+                    .ExcludingMissingMembers();
+                    return options;
+                });
+        }
+
+        private Gift CreateRandomGift()
+        {
+            return new()
+            {
+                Id = Guid.NewGuid(),
+                Name = Guid.NewGuid().ToString(),
+                Price = rand.Next(1000),
+                CreatedDate = DateTimeOffset.UtcNow,
+                UpdatedDate = DateTimeOffset.UtcNow
+            };
         }
     }
 }
