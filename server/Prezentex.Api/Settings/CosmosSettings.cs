3584ec44f5a4274b216c5ab54bdc82a93c6d2b1e
Tomasz Makowski (PGS Software) <tmakowski@pgs-soft.com>
2023-02-10 11:05:06 +0100

    Added CosmosDB provider and DbContext

---
+++ server/Prezentex.Api/Settings/CosmosSettings.cs
@@ -0,0 +1,17 @@
+﻿namespace Prezentex.Api.Settings
+{
+    public class CosmosSettings
+    {
+        public string AccountEndpoint { get; set; }
+        public string AccountKey { get; set; }
+        public string Database { get; set; }
+
+        public string ConnectionString
+        {
+            get
+            {
+                return $"AccountEndpoint={AccountEndpoint};AccountKey={AccountKey}";
+            }
+        }
+    }
+}
