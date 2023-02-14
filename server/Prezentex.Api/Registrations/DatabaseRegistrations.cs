33de94b334ff3bb9db16e8108584f64b5ce2b814
magliarosa <tomekm7@gmail.com>
2023-02-14 13:08:11 +0100

    Added ServiceCollection extensions for Persistence and Auth

---
+++ server/Prezentex.Api/Registrations/DatabaseRegistrations.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Prezentex.Api.Repositories;
+using Prezentex.Api.Repositories.Gifts;
+using Prezentex.Api.Repositories.Recipients;
+using Prezentex.Api.Repositories.Users;
+using Prezentex.Api.Settings;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class DatabaseRegistrations
+    {
+        public static IServiceCollection AddPersistence(
+            this IServiceCollection services,
+            IConfiguration config)
+        {
+            var postgresSettings = config.GetSection(nameof(PostgresSettings)).Get<PostgresSettings>();
+            var cosmosSettings = config.GetSection(nameof(CosmosSettings)).Get<CosmosSettings>();
+
+            services.AddDbContext<EntitiesDbContext>(
+                options => options.UseNpgsql(postgresSettings.ConnectionString));
+
+            services.AddDbContext<CosmosDbContext>(
+                options => options.UseCosmos(cosmosSettings.ConnectionString, cosmosSettings.Database));
+
+            services.AddScoped<IGiftsRepository, PostgresGitsRepository>();
+            services.AddScoped<IRecipientsRepository, PostgresRecipientsRepository>();
+            services.AddScoped<IUsersRepository, PostgresUsersRepository>();
+
+
+            services.AddHealthChecks()
+                .AddNpgSql(
+                postgresSettings.ConnectionString,
+                name: "postgres",
+                timeout: TimeSpan.FromSeconds(3),
+                tags: new[] { "ready" });
+
+            return services;
+        }
+    }
+}
